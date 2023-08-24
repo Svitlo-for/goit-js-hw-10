a8d49b474891bc978f917078ca7d51fd3327e67a
Svitlo-for <serg_machigin@ukr.net>
2023-08-24 23:47:26 +0300

    add-function-hw-10

---
+++ src/cat-api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.thecatapi.com/v1"
+const URL = "https://api.thecatapi.com/v1/breeds";
+const API_KEY = "live_4eRXDHqHvjxZxYAqguMB2rPjbOkQpYGKtdlCcmy1C7h2QkNCBaH5n7c57JPMbYuX";
+
+axios.defaults.headers.common["x-api-key"] = API_KEY;
+
+export function fetchBreeds() {
+  return   
+}
